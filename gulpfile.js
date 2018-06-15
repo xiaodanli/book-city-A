91c1e99bacc46d744810e995fa13251e8961fa5b
lixd <dandan580826@163.com>
2018-06-15 11:34:35 +0800

    “开发环境搭建完成

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,37 @@ var gulp = require('gulp');
 
 var server = require('gulp-webserver');
 
+var sass = require('gulp-sass');
+
+var autoprefixer = require('gulp-autoprefixer');
+
+var sequence = require('gulp-sequence');
+
 var url = require('url');
 
 var path = require('path');
 
 var fs = require('fs');
 
+//开发 --- 编译scss 
+
+gulp.task('devSass', function() {
+    return gulp.src('src/scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            borwsers: ['last 2 versions', 'Android > 4.0']
+        }))
+        .pipe(gulp.dest("src/css"))
+})
+
+//开发 --- 监听scss
+gulp.task('devWatch', function() {
+    return gulp.watch('src/scss/*.scss', ['devSass']);
+})
+
 //起服务
 gulp.task('server', function() {
-    gulp.src('src')
+    return gulp.src('src')
         .pipe(server({
             port: 9090,
             middleware: function(req, res, next) {
@@ -23,4 +45,10 @@ gulp.task('server', function() {
                 res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
             }
         }))
+})
+
+//开发环境
+
+gulp.task('dev', function(cb) {
+    sequence('devSass', 'devWatch', 'server', cb)
 })
